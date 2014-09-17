66d4c8033635b8031791e0aa8190d8ac3c09d9d2
Rougin Gutib <rougingutib@gmail.com>
2014-09-18 00:20:14 +0800

    Added configuration for pagination.

--- ignite.php
+++ ignite.php
@@ -353,6 +353,43 @@ class MY_Pagination extends CI_Pagination
 
 /**
  * ---------------------------------------------------------------------------------------------
+ * Configuration file for Pagination Class
+ * ---------------------------------------------------------------------------------------------
+ */
+
+$pagination = 
+'<?php if ( ! defined(\'BASEPATH\')) exit(\'No direct script access allowed\');
+
+/**
+ * Pagination configuration
+ */
+
+$config[\'per_page\'] = 5;
+
+/**
+ * Styling the pagination
+ *
+ * The code is below is customized for the Bootstrap CSS/JS Framework
+ * You can also include here your customized CSS designs
+ */
+
+// $config[\'full_tag_open\'] = \'<ul class="pagination">\';
+// $config[\'full_tag_close\'] =\'</ul>\';
+// $config[\'num_tag_open\'] = \'<li>\';
+// $config[\'num_tag_close\'] = \'</li>\';
+// $config[\'cur_tag_open\'] = \'<li class="disabled"><li class="active"><a href=#>\';
+// $config[\'cur_tag_close\'] = \'<span class="sr-only"></span></a></li>\';
+// $config[\'next_tag_open\'] = \'<li>\';
+// $config[\'next_tagl_close\'] = \'</li>\';
+// $config[\'prev_tag_open\'] = \'<li>\';
+// $config[\'prev_tagl_close\'] = \'</li>\';
+// $config[\'first_tag_open\'] = \'<li>\';
+// $config[\'first_tagl_close\'] = \'</li>\';
+// $config[\'last_tag_open\'] = \'<li>\';
+// $config[\'last_tagl_close\'] = \'</li>\';';
+
+/**
+ * ---------------------------------------------------------------------------------------------
  * Files and folder to be deleted
  * ---------------------------------------------------------------------------------------------
  */
@@ -471,9 +508,13 @@ file_put_contents('application/config/config.php', $file);
  */
 
 $file = fopen('application/libraries/MY_Pagination.php', 'wb');
+$pagination_config = fopen('application/config/pagination.php', 'wb');
 chmod('application/libraries/MY_Pagination.php', 0777);
+chmod('application/config/pagination.php', 0777);
 file_put_contents('application/libraries/MY_Pagination.php', $my_pagination);
+file_put_contents('application/config/pagination.php', $my_pagination);
 fclose($file);
+fclose($pagination_config);
 
 $routes = file_get_contents('application/config/routes.php');
 $search = '$route[\'default_controller\'] = \'welcome\';
