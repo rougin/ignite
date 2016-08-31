3150c5207141fcc8e8dcf9c931f938e2eb7a54cd
Rougin Royce Gutib <rougingutib@gmail.com>
2016-09-01 00:07:03 +0800

    Update application to v3.1.0

--- application/views/errors/cli/error_db.php
+++ application/views/errors/cli/error_db.php
@@ -1,7 +1,8 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$color = new Colors\Color;
-
-echo $color('Database error: ')->white->bold->bg_red;
-echo $color($heading)->white->bg_red . PHP_EOL;
-echo $color(str_replace("\t", '', $message))->red . PHP_EOL;
+echo "\nDatabase error: ",
+	$heading,
+	"\n\n",
+	$message,
+	"\n\n";
